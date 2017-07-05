d23bb4f6521d335be1f84a2fa604d11551e56a94
rafaelqcosta <rafaqcosta@gmail.com>
2017-07-05 16:55:06 -0300

    FASE 2

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -13,25 +13,76 @@ app.controller("indexController", function($scope, $http) {
 
 	$scope.alunos = [];
 	$scope.aluno = {};
-	$scope.carregarAluno = function() {
+
+	$scope.salvar = function(){
+			var a = $scope.aluno;
+			if (a.id == null){
+				$scope.add();
+			}
+			else{
+				$scope.editar();
+			}
+	};
+	
+	$scope.preencher(){
+		//preencher
+	}
+	
+	$scope.carregar = function() {
 		$http({
 			method : 'GET',
 			url : 'http://localhost:8080/alunos'
 		}).then(function sucesso(response) {
 			$scope.alunos = response.data;
+			console.log(response.data);
+			console.log(response.status);
 		}, function erro(response) {
 			console.log(response.data);
 			console.log(response.status);
 		});
 	};
 
-	$scope.addAluno = function() {
+	$scope.add = function() {
 		$http({
 			method : 'POST',
 			url : 'http://localhost:8080/alunos',
-			data : aluno
+			data : $scope.aluno
 		}).then(function sucesso(response) {
-			// $scope.alunos = response.data;
+			$scope.carregar();
+			// $scope.alunos.push(response.data);
+			console.log(response.data);
+			console.log(response.status);
+		}, function erro(response) {
+			console.log(response.data);
+			console.log(response.status);
+		});
+	};
+
+	$scope.excluir = function(a) {
+		$http({
+			method : 'DELETE',
+			url : 'http://localhost:8080/alunos/' + a.id
+		}).then(function sucesso(response) {
+			// var pos = $scope.alunos.indexOf(a);
+			// $scope.alunos.splice(pos, 1);
+			$scope.carregar();
+			console.log(response.data);
+			console.log(response.status);
+		}, function erro(response) {
+			console.log(response.data);
+			console.log(response.status);
+		});
+	};
+
+	$scope.editar = function(a) {
+		$http({
+			method : 'PUT',
+			url : 'http://localhost:8080/alunos',
+			data : $scope.aluno
+		}).then(function sucesso(response) {
+			$scope.carregar();
+			console.log(response.data);
+			console.log(response.status);
 		}, function erro(response) {
 			console.log(response.data);
 			console.log(response.status);
